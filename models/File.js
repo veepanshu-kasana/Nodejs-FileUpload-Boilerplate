fee6c2a9036219170fe84383534ac80e034a9585
veepanshu-kasana <veepanshukasana2728@gmail.com>
2024-12-14 19:26:16 +0530

    feat: Implement email functionality using Nodemailer

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,6 +16,35 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 });
+
+// Post Middleware
+fileSchema.post("save", async function(doc) {
+    try{
+        console.log("DOC",doc)
+
+        // Transporter
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        // Send Mail
+        let info = await transporter.sendMail({
+            from:`Veepanshu Kasana`,
+            to: doc.email,
+            subject: "File Uploaded",
+            html: `<h2>Hello Jee</h2> <p>New File Uploaded on Cloudinary, View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
+        })
+
+        console.log("INFO", info);
+    }
+    catch(error){
+        console.log(error);
+    }
+})
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
